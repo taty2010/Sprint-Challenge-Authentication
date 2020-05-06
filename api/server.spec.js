6584de9b473e16b5039be5b9410b9e2a0676fcf0
taty2010 <shydalys123@gmail.com>
2020-05-05 19:29:30 -0500

    added tests to test the login and registration post

--- api/server.spec.js
+++ api/server.spec.js
@@ -16,6 +16,15 @@ describe('test server', () => {
       const res = await request(server).post('/api/auth/register');
       expect(res.type).toBe('text/html');
     })
+
+    //NEW TEST
+    it('test user registration', () => {// Not part of original test submission
+      request(server)
+      .post('/api/auth/register')
+      .send({username: 'hello', password: 'password'})
+      .set('Accept', 'application/json')
+      .expect(200)
+    })
     // it('return 500 error message', async () => {
     //   const res = await request(server).post('/api/auth/register');
     //   expect(res.body).toEqual({"message": "Could not register, please enter valid information"})
@@ -30,6 +39,27 @@ describe('test server', () => {
     it('should return JSON type', async () => {
       const res = await request(server).post('/api/auth/login');
       expect(res.type).toBe('application/json');
+    })
+
+    // NEW TESTS
+    it('test user login', async () => {// Not part of original test submission
+      request(server)
+      .post('/api/auth/login')
+      .send({username: 'hello', password: 'password'})
+      .expect(200)
+      .then( res => {
+        expect(res.body.message).toEqual("Welcome back to Dad Jokes hello")
+      })
+    })
+    it('test failed user login', () => {// Not part of original test submission
+      request(server)
+      .post('/api/auth/login')
+      .send({username: 'fail', password: 'password'})
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .then( res => {
+        expect(res.body).toEqual({"message": "Invalid login"})
+      })
     })
   })
 
